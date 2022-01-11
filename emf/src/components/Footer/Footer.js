8f6198f5d40209a28151bc2ad2e133543ea3df75
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-01-11 20:23:44 -0300

    s

--- emf/src/components/Footer/Footer.js
+++ emf/src/components/Footer/Footer.js
@@ -5,10 +5,8 @@ export default function Footer() {
   return (
     <Container>
       <Social>
-        <a href='https://www.facebook.com/NintendoBrazil'><FaFacebookSquare /></a>
-        <a href='https://www.instagram.com/nintendo_brasil'><FaInstagramSquare /></a>
-        <a href='https://twitter.com/NintendoAmerica'><FaTwitterSquare /></a>
-        <a href='https://www.youtube.com/nintendo'><FaYoutubeSquare /></a>
+        <a href='https://pt-br.facebook.com/SamsungBrasil'><FaFacebookSquare /></a>
+           <a href='https://www.youtube.com/samsungbrasil'><FaYoutubeSquare /></a>
       </Social>
       <span>© 2021 JWM(Produtos Eletronicos). Os EletroEletronicos são produtos nacionais de fabricação brasileira.</span>
       <span> Com sua loja fisica localizada em Marabá-PA, Brasil</span>
